9d27b1f15d2b452a4f20554de8a1acec24dc3c93
Muhammad Azmi Adhani <azmi.adhani@shopee.com>
2023-01-15 21:17:00 +0700

    chore: configure react-daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button, Theme } from "react-daisyui";
 
 function App() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <div className="h-52 w-52 bg-black text-white flex justify-center items-center">
+      <div className="h-52 w-52 bg-black text-white flex flex-col justify-center items-center">
         Alright!
+        <Button color="primary">click clock!</Button>
       </div>
     </div>
   );
